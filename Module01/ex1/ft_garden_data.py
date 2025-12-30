122324dc5e6969e2b230dc65d06432a34045eedd
Skade <itzskade@gmail.com>
2025-12-30 10:21:52 +0100

    Update ft_garden_data.py

--- Module01/ex1/ft_garden_data.py
+++ Module01/ex1/ft_garden_data.py
@@ -12,12 +12,12 @@
 # **************************************************************************** #
 
 class Plant:
-    def __init__(self, name, height, age):
+    def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         self.height = height
         self.age = age
 
-def ft_garden_data():
+def ft_garden_data() -> None:
     plant1 = Plant("Rose", 25, 30)
     plant2 = Plant("Sunflower", 80, 45)
     plant3 = Plant("Cactus", 15, 120)
