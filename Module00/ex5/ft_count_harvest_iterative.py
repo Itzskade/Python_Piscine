bcf2f31a5b110b783a4808932ab188f7cdbfecc0
Skade <itzskade@gmail.com>
2026-01-01 18:29:53 +0100

    Update ft_count_harvest_iterative.py

--- Module00/ex5/ft_count_harvest_iterative.py
+++ Module00/ex5/ft_count_harvest_iterative.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
@@ -15,3 +16,6 @@ def ft_count_harvest_iterative() -> None:
     for day in range(1, days + 1):
         print(f"Day {day}")
     print("Harvest time!")
+
+if __name__ == '__main__':
+    ft_count_harvest_iterative()
