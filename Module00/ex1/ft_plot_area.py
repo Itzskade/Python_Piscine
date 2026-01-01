1112445e157a48e63a64a9288c53f77a21e646e2
Skade <itzskade@gmail.com>
2026-01-01 18:36:08 +0100

    Update ft_plot_area.py

--- Module00/ex1/ft_plot_area.py
+++ Module00/ex1/ft_plot_area.py
@@ -15,6 +15,3 @@ def ft_plot_area() -> None:
     length = int(input("Enter length: "))
     width = int(input("Enter width: "))
     print(f"Plot area: {length * width}"))
-
-if __name__ == '__main__':
-    ft_plot_area()
