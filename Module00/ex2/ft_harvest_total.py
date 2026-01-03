d18e55c6051c929ef3f5df43651649b41e9f5ad8
Skade <itzskade@gmail.com>
2026-01-03 09:41:47 +0100

    Update ft_harvest_total.py

--- Module00/ex2/ft_harvest_total.py
+++ Module00/ex2/ft_harvest_total.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_harvest_total.py                                :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: rmarin-n <rmarin-n@student.42barcelona.co  +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/12/26 08:25:11 by rmarin-n          #+#    #+#              #
-#    Updated: 2025/12/26 08:25:13 by rmarin-n         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_harvest_total() -> None:
     first = int(input("Day 1 harvest: "))
     second = int(input("Day 2 harvest: "))
