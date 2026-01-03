ba27635518bc45eb38a8f1bd59944653d03f721c
Skade <itzskade@gmail.com>
2026-01-03 09:20:28 +0100

    Update ft_first_exception.py

--- Module02/ex0/ft_first_exception.py
+++ Module02/ex0/ft_first_exception.py
@@ -8,12 +8,12 @@ def check_temperature(temp_str: str) -> None:
         print(f"Error: {temp_str} no es un número")
         return None
 
-    if tmp < 0:
-        print(f"Error: {tmp}ºC is too cold for plants (min 0ºC)\n")
-    elif tmp > 40:
-        print(f"Error: {tmp}ºC is too hot for plants (max 40ºC)\n")
+    if temp < 0:
+        print(f"Error: {temp}ºC is too cold for plants (min 0ºC)\n")
+    elif temp > 40:
+        print(f"Error: {temp}ºC is too hot for plants (max 40ºC)\n")
     else:
-        print(f"Temperature {tmp}ºC is perfect for plants!\n")
+        print(f"Temperature {temp}ºC is perfect for plants!\n")
 
 
 def ft_first_exception():
