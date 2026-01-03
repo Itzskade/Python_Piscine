e50ecc021f605b5f5c843361a9e2d009a2174854
Skade <itzskade@gmail.com>
2026-01-03 09:42:04 +0100

    Update ft_water_reminder.py

--- Module00/ex4/ft_water_reminder.py
+++ Module00/ex4/ft_water_reminder.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_water_reminder.py                               :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: rmarin-n <rmarin-n@student.42barcelona.co  +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/12/26 08:25:34 by rmarin-n          #+#    #+#              #
-#    Updated: 2025/12/26 08:25:35 by rmarin-n         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_water_reminder() -> None:
     days =  int(input(f"Days since last watering: "))
     if (days > 2):
