28a9bda7cc09d04914b5dc62bd47578f8cfbc0bc
Skade <itzskade@gmail.com>
2026-01-03 09:42:28 +0100

    Update ft_garden_summary.py

--- Module00/ex6/ft_garden_summary.py
+++ Module00/ex6/ft_garden_summary.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_garden_summary.py                               :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: rmarin-n <rmarin-n@student.42barcelona.co  +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/12/26 08:26:08 by rmarin-n          #+#    #+#              #
-#    Updated: 2025/12/26 08:26:09 by rmarin-n         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_garden_summary() -> None:
     name = input("Enter garden name: ")
     nplants = input("Enter number of plants: ")
