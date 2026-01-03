f8b8b45ddf04845b475f182b751b39ed1dc788bd
Skade <itzskade@gmail.com>
2026-01-03 09:42:37 +0100

    Update ft_seed_inventory.py

--- Module00/ex7/ft_seed_inventory.py
+++ Module00/ex7/ft_seed_inventory.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_seed_inventory.py                               :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: rmarin-n <rmarin-n@student.42barcelona.co  +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/12/26 08:26:18 by rmarin-n          #+#    #+#              #
-#    Updated: 2025/12/26 08:26:22 by rmarin-n         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     seed_name = seed_type.capitalize()
     msg = f"{seed_name} seeds: {quantity} {unit}"
