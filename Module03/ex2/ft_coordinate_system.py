8ce8ebea5add5c4e94374b5ea3ab5ed3ac715623
Skade <itzskade@gmail.com>
2026-01-03 10:27:47 +0100

    Update ft_coordinate_system.py

--- Module03/ex2/ft_coordinate_system.py
+++ Module03/ex2/ft_coordinate_system.py
@@ -56,5 +56,6 @@ def ft_coordinate_system() -> None:
         print(f"Player at x={x}, y={y}, z={z}")
         print(f"Coordinates: X={x}, Z={z}, Y={y}")
 
+
 if __name__ == "__main__":
     ft_coordinate_system()
