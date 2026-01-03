a12201effd792ee97a8d6fb94b0a312d99234ed7
Skade <itzskade@gmail.com>
2026-01-03 09:42:14 +0100

    Update ft_count_harvest_iterative.py

--- Module00/ex5/ft_count_harvest_iterative.py
+++ Module00/ex5/ft_count_harvest_iterative.py
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_count_harvest_iterative.py                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: rmarin-n <rmarin-n@student.42barcelona.co  +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/12/26 08:25:52 by rmarin-n          #+#    #+#              #
-#    Updated: 2025/12/26 08:25:52 by rmarin-n         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 def ft_count_harvest_iterative() -> None:
     days = int(input("Days until harvest: "))
     for day in range(1, days + 1):
