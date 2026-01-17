651d67a1f15cf93a4d9bc68e2c8418ce5b034b8c
Skade <itzskade@gmail.com>
2026-01-17 20:57:41 +0100

    Delete Module07 directory

--- Module07/ex2/__init__.py
+++
@@ -1,7 +0,0 @@
-from ex2.Combatable import Combatable
-from ex2.Magical import Magical
-from ex2.EliteCard import EliteCard
-
-__author__ = 'rmarin-n'
-__version__ = '1.0.0'
-__all__ = ['Combatable', 'Magical', 'EliteCard']
