9a11d8a5234ff2f8f325910e0cc7b2a78fe5e7de
Skade <itzskade@gmail.com>
2026-01-17 18:16:08 +0100

    Delete Module07 directory

--- Module07/ex4/main.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print("=== DataDeck Tournament Platform ===\n")
-    print("Registering Tournament Cards...\n")
-
-    print("\n=== Tournament Platform Successfully Deployed! ===")
-    print("All abstract patterns working together harmoniously!")
-
-if __name__ == '__main__':
-    main()
